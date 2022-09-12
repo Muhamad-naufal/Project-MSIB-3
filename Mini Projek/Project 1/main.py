23742a77911f9797eb800f8af2848c8543cf7ec9
Muhamad-naufal <muhamadnaufal594@gmail.com>
2022-09-12 11:33:22 +0700

    update project mini

--- Mini Projek/Project 1/main.py
+++ Mini Projek/Project 1/main.py
@@ -1,14 +1,20 @@
+#import library speech_recognition
 import speech_recognition as sr
+
+#pendefinisian
 engine = sr.Recognizer()
 mic = sr.Microphone()
 hasil = ''
-engine.pause_threshold = 1
+engine.pause_threshold = 0.5
 
+#input dan proses
 with mic as source:
+    #inputan
     print("Perintah Anda....")
     rekaman = engine.listen(source)
     print("Kehabisan Waktu")
 
+    #proses
     try:
         hasil = engine.recognize_google(rekaman, language= "id-ID")
         print(hasil)
@@ -17,6 +23,7 @@ with mic as source:
     except Exception as e:
         print(e)
 
+#output
 text_file = open("Hasil.txt", "w")
 text_file.write(hasil)
 text_file.close()
